6ee9ed8e7ab13ec0e93de92ef7a874b9135cfd11
Luke Segars <luke@lukesegars.com>
2017-01-19 10:28:09 -0500

    Added actual stuff.

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+window.addEventListener('load', function () {
+    // Put this anywhere; normal scope rules will apply
+    let testModule = require('./module'); // import module.js
+
+    console.log('ready to rock ');
+});
